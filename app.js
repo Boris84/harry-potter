32886fad63ea307e977c6df2b0d655ac7c51e612
Boris84 <simon.789@hotmail.co.uk>
2021-01-14 09:51:53 +0000

    Harry Potter API

---
+++ app.js
@@ -0,0 +1,68 @@
+let characterList = document.getElementById("charactersList")
+let hpCharacters = [];
+
+
+// get user input a filter search results 
+
+document.querySelector('input').addEventListener('keyup', (event) => {
+ const searchString = event.target.value.toLowerCase();
+    
+ const filteredCharacters = hpCharacters.filter((character) => { 
+    return character.name.toLowerCase().includes(searchString); 
+  });
+      displayCharacters(filteredCharacters);
+});
+
+
+
+
+
+
+// AJAX xhr request to Harry Potter API 
+
+const loadCharacters = function() {
+    
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://hp-api.herokuapp.com/api/characters', true);
+xhr.onload = function() {
+    
+  if (this.status === 200) {
+    hpCharacters = JSON.parse(this.responseText);
+    displayCharacters(hpCharacters);
+  }
+}
+    xhr.send();
+}
+
+
+
+
+// formatting response object in browser 
+function displayCharacters(hpCharacters) {
+  
+let output = '';
+  
+hpCharacters.forEach((character) => { 
+      
+    output += 
+      '<ul>' +
+        `<img src="${character.image}" width="180px" height="200px">` +
+        `<li>Name: ${character.name}</li>` +
+        `<li>Actor: ${character.actor}</li>` + 
+        `<li>DOB: ${character.dateOfBirth}</li>` +
+        `<li>House: ${character.house}</li>` +
+      '</ul>';
+  });
+    
+  document.getElementById("charactersList").innerHTML = output;
+
+}
+
+loadCharacters();
+
+
+
+
+
+
+
